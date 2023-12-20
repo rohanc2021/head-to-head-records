f03eb3ccdf6898ba704c9bf7d89cd5454e7c97ef
rohanchotirmall <rohanc2021@outlook.com>
2023-12-20 17:34:18 -0500

    Bug fixes/documentation

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import json
+from typing import Any
+
 import pandas as pd
 
 
-def format_data(raw_data: dict) -> dict[str, list[int]]:
+def format_data(raw_data: dict) -> dict[Any, list[Any]]:
+    """
+    Iterates through the data for each team, building a dictionary mapping
+    team names to their number of wins against each opponent provided.
+
+    :param raw_data: raw_data: data for each team, containing Win/Loss records for each team
+    :return: dictionary mapping team names to their number of wins against each opponent provided
+    """
     formatted_data = {}
     team1_index = 0  # Keeps track of which team we are iterating through WL data for
     # Iterate through data for each team
@@ -31,12 +40,10 @@ if __name__ == '__main__':
     # Get formatted data and column names (list of teams)
     win_data = format_data(data)
     teams_list = list(win_data.keys())
+    win_data["Tm"] = teams_list  # Add team names to bottom
 
     # Create table
     df = pd.DataFrame.from_dict(data=win_data, orient='index', columns=teams_list)
-    # Add columns to bottom of table
-    df = df._append(pd.Series(name='Tm'))
-    df.iloc[-1] = df.columns
     # Add tm to upper left
     df.columns.name = 'Tm'
 
